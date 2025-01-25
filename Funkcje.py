23f98f60bc30bd9dccf2282b41b0cba9c5110378
MateuszZurkowski <mat.zurkowski@gmail.com>
2025-01-25 11:52:41 +0100

    Dodanie funkcja.py

---
+++ Funkcje.py
@@ -0,0 +1,15 @@
+#Funkcje
+#Wypisz: jabłko , gruszka, pietruszka
+
+#   I metoda:
+def wypisz_3():
+    print("jabłko")
+    print("gruszka")
+    print("pietruszka")
+
+wypisz_3()
+
+
+#   II metoda: Wywołanie funkcji:
+
+
